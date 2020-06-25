da0ac75fd2cc03d2c74fbcf6a031897589058fe5
Andreas Willich <andreas@willich.io>
2020-06-25 14:30:09 +0200

    adjust look

--- conf.py
+++ conf.py
@@ -50,9 +50,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_theme_options = {
-    'canonical_url':'https://docs.specflow.org'
+    'canonical_url':'https://docs.specflow.org',
+    'display_version':'false',
+    'logo_only': True
 }
 html_logo = '_static/logo.png'
 html_css_files = [
-    'css/custom.css',
+    'css/custom.css'
 ]
